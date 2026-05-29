5577d5fdf51aa643fb8d35594a122bb19c7bb90b
Janmendu prakash dhal <janmendudhal@gmail.com>
2026-05-29 21:21:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// problem 1 for let //
+let price = 100;
+price = 200;
+console.log(price);
+// problem 2 for let //
+let name = "John";
+name = "Doe";
+console.log(name);
+// problem 3 for let //
+let age = 30;
+age = 31;
+console.log(age);
+// problem 4 for let //
+let city = "New York";
+city = "Los Angeles";
+console.log(city);
+// problem 5 for let //
+let country = "USA";
+country = "India";
+console.log(country);
