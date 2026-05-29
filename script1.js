ee70ad27493115d70fe328709bb755137c2dc1ff
Janmendu prakash dhal <janmendudhal@gmail.com>
2026-05-30 00:18:21 +0530

    On branch main

---
+++ script1.js
@@ -0,0 +1,28 @@
+// problem 1 for const //
+const user = {
+   name: "John"
+};
+user.name = "Alex";
+console.log(user.name);
+// problem 2 for const //
+const numbers = [1, 2, 3];
+numbers.push(4);
+console.log(numbers);
+// problem 3 for const //
+const person = {
+    name: "Janmendu",
+    age: 25
+};
+person.age = 26;
+console.log(person.age);
+// problem 4 for const //
+const colors = ["red", "green", "blue"];
+colors[0] = "yellow";
+console.log(colors);
+// problem 5 for const //
+const car = {
+    make: "Toyota",
+    model: "Camry"
+};
+car.model = "Corolla";
+console.log(car.model);
